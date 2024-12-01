41f8cc32705aa7b410351951e877f23f1a427dc7
skiletro <19377854+skiletro@users.noreply.github.com>
2024-12-01 19:29:26 +0000

    (feat): init

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        go
+        gopls
+      ];
+    };
+  };
+}
